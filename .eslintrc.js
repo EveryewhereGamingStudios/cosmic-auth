2c39c9f03572e3cdb269825fde17de443b3baad9
Alexandre Ferreira <alexjorgef@protonmail.com>
2023-01-26 20:25:54 +0000

    fix deploy to cyclic
    Signed-off-by: Alexandre Ferreira <alexjorgef@protonmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   extends: ['@moralisweb3'],
-  ignorePatterns: ['**/lib/**/*'],
+  ignorePatterns: ['**/dist/**/*'],
 };
---
+++ lib/config.d.ts
@@ -0,0 +1,10 @@
+declare const _default: Readonly<{
+    PORT: number;
+    SUPABASE_PUBLIC_ANON_KEY: string;
+    SUPABASE_URL: string;
+    APP_NAME: string;
+    MORALIS_API_KEY: string;
+    SUPABASE_JWT: string;
+    SUPABASE_SERVICE_KEY: string;
+} & import("envalid").CleanedEnvAccessors>;
+export default _default;
